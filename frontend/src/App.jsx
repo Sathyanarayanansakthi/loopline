a6b635df06a705e78c220f9755cd12bdfab20042
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2025-06-22 11:24:05 +0530

    Landing Page Error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import LandingPage from './pages/landingPage'
+import LoginPage from './pages/Loginpage'
+import LandingPage from './pages/LandingPage'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <LandingPage/>
-      
-    </div>
+<BrowserRouter>
+<Routes>
+   <Route path="/" element={<LandingPage />} />
+  <Route path="/login" element={<LoginPage />} /> 
+</Routes>
+</BrowserRouter>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import LoginPage from './pages/Loginpage'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<LoginPage />} /> 
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
